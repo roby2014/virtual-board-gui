21a6d1321a97b8e388433276b879ff2ec4c783a6
awyxx <robyawyx@gmail.com>
2022-11-13 21:09:20 +0000

    added seven segment display and other fixes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -27,6 +27,8 @@ import androidx.compose.ui.window.application
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
+import org.http4k.client.WebsocketClient
+import org.http4k.core.Uri
 import org.http4k.websocket.Websocket
 import org.http4k.websocket.WsMessage
 import java.awt.FileDialog
@@ -40,12 +42,54 @@ data class IOComponent(
 )
 
 @Serializable
+data class SevSeg(
+    var a: String = "",
+    var b: String = "",
+    var c: String = "",
+    var d: String = "",
+    var e: String = "",
+    var f: String = "",
+    var g: String = "",
+    var p: String = ""
+)
+
+@Serializable
 data class Board(
     val boardName: String = "Virtual Board",
     val leds: List<IOComponent> = listOf<IOComponent>(),
     val switches: List<IOComponent> = listOf<IOComponent>(),
-    val buttons: List<IOComponent> = listOf<IOComponent>()
+    val buttons: List<IOComponent> = listOf<IOComponent>(),
+    val otherPins: List<IOComponent> = listOf<IOComponent>(),
+    val sevseg: List<SevSeg> = listOf<SevSeg>()
 )
+
+fun Board.getPinValue(pinId: String): MutableState<Boolean> {
+    if (pinId.isEmpty())
+        return mutableStateOf(false)
+
+    this.leds.forEach { led ->
+        if (led.pinId == pinId) {
+            return led.status
+        }
+    }
+    this.switches.forEach { sw ->
+        if (sw.pinId == pinId) {
+            return sw.status
+        }
+    }
+    this.buttons.forEach { btn ->
+        if (btn.pinId == pinId) {
+            return btn.status
+        }
+    }
+    this.otherPins.forEach { p ->
+        if (p.pinId == pinId) {
+            return p.status
+        }
+    }
+
+    throw Exception("could not find '$pinId' (seven segment linked pin)")
+}
 
 fun openFileDialog(
     window: ComposeWindow,
@@ -70,73 +114,119 @@ fun openFileDialog(
     }.files.toSet()
 }
 
-fun main(args: Array<String>) = application {
-    val board = remember { mutableStateOf(Board()) }
-    val boardLoaded = remember { mutableStateOf(false) }
-    val wsConnected = remember { mutableStateOf(false) }
-
+fun main(args: Array<String>) {
     var ws: Websocket? = null
-    val wsHandler = { conn: Websocket ->
-        conn.run {
-            wsConnected.value = true
-        }
-        conn.onMessage { it ->
-            if (boardLoaded.value) {
-                val msg = it.bodyString()
-                if (msg.contains("[ERROR]")) {
-                    println(it.bodyString())
-                    return@onMessage
-                }
 
-                val words = msg.split(" ")
-                val pinId = words[0]
-                val newValue = words[2]
-                // the gui app only receives output led pins and hex
-                val ledIdx = board.value.leds.indexOfFirst { led -> led.pinId == pinId }
-                if (ledIdx != -1) {
-                    board.value.leds[ledIdx].status.value = newValue == "1"
+    application {
+
+        //val board = remember { mutableStateOf(Board()) }
+        //val boardLoaded = remember { mutableStateOf(false) }
+        val board = remember { mutableStateOf(Json.decodeFromString<Board>
+            (File("/home/roby/repos/virtual-board-gui/src/main/resources/board.json").readText())) }
+        val boardLoaded = remember { mutableStateOf(true) }
+
+        val wsConnected = remember { mutableStateOf(false) }
+        val sevSegValues = remember { mutableStateOf(mutableListOf(listOf(false))) }
+        val sevSegValue = List(board.value.sevseg.size) {
+            List(8) { mutableStateOf(false) }
+        }
+
+        board.value.sevseg.forEachIndexed { idx, sev ->
+            sevSegValue[idx][0].value = remember { board.value.getPinValue(sev.a) }.value
+            sevSegValue[idx][1].value = remember { board.value.getPinValue(sev.b) }.value
+            sevSegValue[idx][2].value = remember { board.value.getPinValue(sev.c) }.value
+            sevSegValue[idx][3].value = remember { board.value.getPinValue(sev.d) }.value
+            sevSegValue[idx][4].value = remember { board.value.getPinValue(sev.e) }.value
+            sevSegValue[idx][5].value = remember { board.value.getPinValue(sev.f) }.value
+            sevSegValue[idx][6].value = remember { board.value.getPinValue(sev.g) }.value
+            sevSegValue[idx][7].value = remember { board.value.getPinValue(sev.p) }.value
+        }
+
+        val wsHandler = {
+            val conn = WebsocketClient.nonBlocking(Uri.of("ws://127.0.0.1:8083")) {
+                it.run {
+                    println("runn")
+                    ws = it
+                    wsConnected.value = true
+                    board.value.switches.forEach { sw ->
+                        // TODO: do we want to reset all switches when connecting to simul
+                        send(WsMessage("CHANGE ${sw.pinId} ${if (sw.status.value) 1 else 0}"))
+                    }
                 }
             }
-        }
-        conn.onClose {
-            wsConnected.value = false
-            ws = null
-        }
+            conn.onMessage { it ->
+                if (boardLoaded.value) {
+                    val msg = it.bodyString()
+                    if (msg.contains("[ERROR]")) {
+                        println(it.bodyString())
+                        return@onMessage
+                    }
 
-    }
+                    val words = msg.split(" ")
+                    val pinId = words[0]
+                    val newValue = words[2]
 
-    Window(
-        onCloseRequest = ::exitApplication,
-        title = board.value.boardName
-    ) {
-        MaterialTheme {
-            menuBar {
-                val files = openFileDialog(window, "Load Board Configuration", listOf("json"))
-                if (files.isNotEmpty()) {
-                    val fp = files.first().readText()
-                    board.value = Json.decodeFromString<Board>(fp)
-                    boardLoaded.value = true
+                    // the gui app only receives output led pins and hex(otherpins)
+                    val ledIdx = board.value.leds.indexOfFirst { led -> led.pinId == pinId }
+                    if (ledIdx != -1) {
+                        board.value.leds[ledIdx].status.value = newValue == "1"
+                    } else {
+                        val pinIdx = board.value.otherPins.indexOfFirst { p -> p.pinId == pinId }
+                        if (pinIdx != -1) {
+                            board.value.otherPins[pinIdx].status.value = newValue == "1"
+                        }
+                    }
+
                 }
             }
+            conn.onClose {
+                wsConnected.value = false
+                ws = null
+                println("closing websocket connection")
+            }
 
-            if (!boardLoaded.value) {
-                showInitialWindow()
-            } else {
-                Column {
-                    ledsRow(board.value.leds)
-                    switchesRow(board.value.switches) { pinId, value ->
-                        ws?.send(WsMessage("CHANGE $pinId $value"))
+        }
+
+        Window(
+            onCloseRequest = ::exitApplication,
+            title = board.value.boardName
+        ) {
+            MaterialTheme {
+                menuBar {
+                    val files = openFileDialog(window, "Load Board Configuration", listOf("json"))
+                    if (files.isNotEmpty()) {
+                        val fp = files.first().readText()
+                        board.value = Json.decodeFromString<Board>(fp)
+                        boardLoaded.value = true
                     }
-                    buttonsRow(board.value.buttons) { pinId, value ->
-                        ws?.send(WsMessage("CHANGE $pinId $value"))
-                    }
-                    Row {
-                        Button(onClick = {
-                            if (ws == null) {
-                                ws = connectToSimulation(wsHandler)
+                }
+
+                if (!boardLoaded.value) {
+                    showInitialWindow()
+                } else {
+                    Column {
+                        // output
+                        sevsegRow(sevSegValue)
+                        ledsRow(board.value.leds)
+
+                        // input
+                        switchesRow(board.value.switches) { pinId, value ->
+                            ws?.send(WsMessage("CHANGE $pinId $value"))
+                        }
+
+                        buttonsRow(board.value.buttons) { pinId, value ->
+                            ws?.send(WsMessage("CHANGE $pinId $value"))
+                        }
+
+                        // connect to simulation button
+                        Row {
+                            Button(onClick = {
+                                if (ws == null) {
+                                    wsHandler()
+                                }
+                            }) {
+                                Text(if (wsConnected.value) "Connected" else "Connect to Simulation")
                             }
-                        }) {
-                            Text(if (wsConnected.value) "Connected" else "Connect to Simulation")
                         }
                     }
                 }
@@ -166,6 +256,51 @@ fun showInitialWindow() {
             modifier = Modifier.fillMaxSize().padding(25.dp)
         )
     }
+}
+
+@Composable
+fun sevsegRow(sevSegValue: List<List<MutableState<Boolean>>>) {
+    Row {
+        sevSegValue.forEachIndexed { idx, seg ->
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .size(100.dp)
+            ) {
+                Row {
+                    Text("HEX$idx")
+                }
+                Row {
+                    Box {
+                        Image(
+                            painter = painterResource("7seg_base.png"),
+                            contentDescription = "Switch Image",
+                        )
+                        seg.forEachIndexed { index, mutableState ->
+                            if (mutableState.value) {
+                                if (index == 7) {
+                                    showSevSegNode('p')
+                                } else {
+                                    showSevSegNode('a' + index)
+                                }
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
+
+
+    }
+}
+
+@Composable
+fun showSevSegNode(node: Char) {
+    Image(
+        painter = painterResource("7seg_$node.png"),
+        contentDescription = "Seven Segment $node",
+    )
 }
 
 @Composable
--- src/main/kotlin/Simulation.kt
+++ src/main/kotlin/Simulation.kt
@@ -1,9 +1,14 @@
 import org.http4k.client.WebsocketClient
 import org.http4k.core.Uri
 import org.http4k.websocket.Websocket
+import org.http4k.websocket.WsMessage
 
-fun connectToSimulation(block: (Websocket) -> Unit): Websocket {
-    val nonBlockingClient = WebsocketClient.nonBlocking(Uri.of("ws://127.0.0.1:8083"))
-    block(nonBlockingClient)
-    return nonBlockingClient
+fun connectToSimulation(block: (Websocket) -> Unit, switches: List<IOComponent>): Websocket {
+    val conn = WebsocketClient.nonBlocking(Uri.of("ws://127.0.0.1:8083")) {
+        it.run {
+
+        }
+    }
+    block(conn)
+    return conn
 }
